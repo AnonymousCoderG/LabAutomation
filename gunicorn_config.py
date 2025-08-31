62db2dcc97bd1e43a56c180e7aaa8b5a78a19218
AnonymousCoderG <gourivpawar@gmail.com>
2025-08-31 11:43:38 +0530

    updated gunicorn_config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,18 +1,4 @@
-# gunicorn_config.py
-
-# This is the most important setting.
-# It tells Gunicorn to use the eventlet worker, which is designed for
-# asynchronous applications like those using Socket.IO.
 worker_class = 'eventlet'
-
-# This setting tells the worker how many concurrent requests it can handle.
-# For eventlet, this can be a higher number.
 worker_connections = 1000
-
-# The number of worker processes. For Render's free tier, 
-# keeping this at 1 is the most stable option to conserve memory.
 workers = 1
-
-# The timeout for workers. If a worker is silent for this long, it's killed.
-# We increase it slightly to be safe.
 timeout = 60
